20e65c38c5bdf7ed6d244f889be997cef920607d
Ali foroozan <88268577+alifrn51@users.noreply.github.com>
2026-09-22 11:17:36 +0330

    Init commit

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm") version "2.4.10"
+    kotlin("plugin.serialization") version "2.4.20"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
+}
+
+kotlin {
+    jvmToolchain(26)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
